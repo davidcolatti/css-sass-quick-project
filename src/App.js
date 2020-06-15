72b2e125e8411ed59dea935aad97ca47e72a99a7
DavidColatti <davidcolatti@gmail.com>
2020-06-15 16:48:07 -0400

    feat: setup of app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.scss";
+
+import React from "react";
+
+const App = () => {
+  return (
+    <main class="container">
+      <div class="outer-layer">
+        <div class="inner-layer">
+          <div class="top-half">TOP HALF</div>
+          <div class="bottom-half">
+            <p>
+              Stock images and videos that help spread the message to support
+              social distancing.
+            </p>
+            <span class="browse-tab">Browse</span>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
