052f356ce78afff13ea9fd994ff6da6af1069e19
DavidColatti <davidcolatti@gmail.com>
2020-06-17 15:45:51 -0400

    feat: media queries and transition, improvement in jsx aswell

--- src/App.js
+++ src/App.js
@@ -5,24 +5,21 @@ import React from "react";
 const App = () => {
   return (
     <main className="container">
-      <div className="outer-layer">
-        <div className="inner-layer">
-          <div className="top-half">
-            <span className="social-tab">
-              <h1>SOCIAL DISTANCING</h1>
-            </span>
-            <div class="arrow-up"></div>
-          </div>
-          <div className="bottom-half">
-            <h2>
-              Stock images and videos that help spread the message to support
-              social distancing.
-            </h2>
-            <span className="browse-tab">
-              <i className="icon fa fa-clone" aria-hidden="true"></i>
-              <h2 className="browse-h2">Browse</h2>
-            </span>
-          </div>
+      <div className="inner-layer">
+        <div className="top-half">
+          <span className="social-tab">
+            <h1>SOCIAL DISTANCING</h1>
+          </span>
+        </div>
+        <div className="bottom-half">
+          <h2 className="content">
+            Stock images and videos that help spread the message to support
+            social distancing.
+          </h2>
+          <span className="browse-tab">
+            <i className="icon fa fa-clone" aria-hidden="true"></i>
+            <h2 className="browse-h2">Browse</h2>
+          </span>
         </div>
       </div>
     </main>
